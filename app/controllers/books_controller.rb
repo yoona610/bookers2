407bbfb7f7a277fb92d0c34412d801f7c7aab550
yoona610 <xiaotengxinai@gmail.com>
2021-06-03 05:25:38 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,26 +7,20 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to books_path(current_user.id)
   end
 
   def index
     @books = Book.all
-    @book = Book.new
   end
 
   def show
-    @book = Book.find(params[:id])
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@user.id)
   end
 
   def destroy
