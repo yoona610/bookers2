7796ab93a648930a32c97c172475f63fef65cae5
yoona610 <xiaotengxinai@gmail.com>
2021-06-06 10:47:51 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,21 +4,28 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    redirect_to book_path(book)
+    @user = current_user
+    @books = Book.all
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
+
   end
 
   def index
     @books = Book.all
-    @user = User.find(current_user.id)
+    @user = current_user
     @book = Book.new
   end
 
   def show
     @newbook = Book.new
     @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def edit
@@ -26,9 +33,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,12 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(current_user.id)
-    user.update(user_params)
-    redirect_to user_path(current_user.id)
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
   end
 
   def info
