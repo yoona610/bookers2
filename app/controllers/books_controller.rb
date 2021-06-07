d79dd346ac5a625a70ba11edcfff2bb76f9a295a
yoona610 <xiaotengxinai@gmail.com>
2021-06-07 03:29:10 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -46,7 +46,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path#
+    redirect_to books_path
   end
 
   private
